4dad2ad8cf78e719d190fd703928603e06f22969
Kartik <kartik.mathur10@gmail.com>
2025-12-07 17:53:37 +0530

    FrontEnd

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,47 @@ import React from "react";
 import { useState } from "react";
 import axios from "./api/axios";
 import { useEffect } from "react";
+import { useRef } from "react";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
-
+  const nameRef = useRef();
+  const descriptionRef = useRef();
   useEffect(() => {
     axios.get("/todos").then(({ data }) => {
-      console.log(data);
       setTodos(data);
     });
   }, []);
 
+  function addTodo() {
+    const name = nameRef.current.value;
+    const description = descriptionRef.current.value;
+    if (!name || !description) return;
+    
+    axios
+      .post("/todos", {
+        name,
+        description,
+      })
+      .then(({ data }) => {
+        setTodos(data);
+      });
+  }
+
   return (
     <div>
       <h1>Todos Application</h1>
+
+      <div>
+        <input ref={nameRef} type="text" placeholder="Enter Task Name" />
+        <input
+          ref={descriptionRef}
+          type="text"
+          placeholder="Enter Description"
+        />
+        <button onClick={addTodo}>Add Task</button>
+      </div>
+
       {todos.map((t) => {
         return (
           <li key={t._id}>
